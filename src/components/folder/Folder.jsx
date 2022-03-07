04a49167e770ff5fc2203370bdafceaebf34cfea
Nathalia-Urenha <naty.urenha@gmail.com>
2022-03-06 23:43:22 -0300

    mystory

--- src/components/folder/Folder.jsx
+++ src/components/folder/Folder.jsx
@@ -2,7 +2,7 @@ import './folder.scss';
 
 export default function Folder({path, title}) {
   return (
-    <a className="folder" href={path} target="_blank">
+    <a className="folder" href={path} target="_blank" rel="noopener noreferrer">
         <img src="assets/folderr.png" alt="projeto" />
         <span>{title}</span>
     </a>
--- src/components/myStory/MyStory.jsx
+++ src/components/myStory/MyStory.jsx
@@ -1,30 +1,32 @@
 import './myStory.scss';
-// import TreeView from '@mui/lab/TreeView';
-// import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-// import ChevronRightIcon from '@mui/icons-material/ChevronRight';
-// import TreeItem from '@mui/lab/TreeItem';
+import {useState} from 'react';
+import {ArrowDropDown} from '@material-ui/icons';
 
 export default function MyStory() {
+  const [showElement, setShowElement] = useState(false);
+  const showOrHide = () => setShowElement(!showElement);
+
   return (
     
       <div className="mystory" id="mystory">
       <div className="left">
         <div className="wrapper">
-          <h1>Minha História</h1>
+          <button onClick={showOrHide}>Minha História <ArrowDropDown /></button>
+          { showElement ? 
           <p className="text">
-          Eu sou a Nathália, tenho <span style={{fontWeight: 'bold', color: '#3fafaa'}}>21 anos</span>, e estou no 9º semestre de <span style={{fontWeight: 'bold', color: '#3fafaa'}}>Engenharia de Computação </span>. 
-          Sempre estudei em escola pública e decidi que queria cursar Engenharia no meu 3º ano do Ensino Médio.  
-          Eu tomei essa decisão por algumas razões, dentre elas está a minha paixão por <span style={{fontWeight: 'bold', color: '#3fafaa'}}>tecnologia e matemática.</span>
-          Sempre me destaquei em matemática quando estava na escola, e sempre fui muito curiosa a respeito de tecnologia, 
-          tanto inovações como a própria computação. No final  de 2017, quando eu esteva finalizando o 3º ano do ensino médio 
-          eu comecei a  prestar os vestibulares das universidades estaduais, e  realizei o ENEM. A partir  dele, eu pude ver as 
-          faculdades que eu conseguiria entrar, e lá estava o <span style={{fontWeight: 'bold', color: '#3fafaa'}}>IFSP  - campus Birigui</span>, que era o mais próximo de minha cidade. 
-          Me inscrevi no   SISU, e passei na primeira chamada. Ingressei no início de 2018, e desde então eu não me vejo fazendo 
-          outra coisa que não seja nessa área.
-          Atualmente eu faço  estágio na empresa <span style={{fontWeight: 'bold', color: '#3fafaa'}}>Gorila Invest</span>, atuando como <span style={{fontWeight: 'bold', color: '#3fafaa'}}>desenvolvedora front-end</span>, entrei nesse estágio em 
-          outubro de 2021, e estou aprendendo muitas coisas e  amando o que estou fazendo. Meus planos para o futuro é terminar 
-          minha graduação e continuar nessa área de desenvolvimento web, que foi a que eu mais me identifiquei até agora.
-          </p>
+            Eu sou a Nathália, tenho <span>21 anos</span>, e estou no 9º semestre de <span>Engenharia de Computação </span>. 
+            Sempre estudei em escola pública e decidi que queria cursar Engenharia no meu 3º ano do Ensino Médio.  
+            Eu tomei essa decisão por algumas razões, dentre elas está a minha paixão por <span>tecnologia e matemática.</span>
+            Sempre me destaquei em matemática quando estava na escola, e sempre fui muito curiosa a respeito de tecnologia, 
+            tanto inovações como a própria computação. No final  de 2017, quando eu esteva finalizando o 3º ano do ensino médio 
+            eu comecei a  prestar os vestibulares das universidades estaduais, e  realizei o ENEM. A partir  dele, eu pude ver as 
+            faculdades que eu conseguiria entrar, e lá estava o <span>IFSP  - campus Birigui</span>, que era o mais próximo de minha cidade. 
+            Me inscrevi no   SISU, e passei na primeira chamada. Ingressei no início de 2018, e desde então eu não me vejo fazendo 
+            outra coisa que não seja nessa área.
+            Atualmente eu faço  estágio na empresa <span>Gorila Invest</span>, atuando como <span>desenvolvedora front-end</span>, entrei nesse estágio em 
+            outubro de 2021, e estou aprendendo muitas coisas e  amando o que estou fazendo. Meus planos para o futuro é terminar 
+            minha graduação e continuar nessa área de desenvolvimento web, que foi a que eu mais me identifiquei até agora.
+          </p> : null }
         </div>
 
       </div>
